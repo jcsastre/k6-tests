4730bf9298b5deaf52fb2e6ed9157b103210c85b
Juan Carlos Sastre <juan.carlos.sastre.iglesias@gmail.com>
2020-06-02 11:02:40 +0200

    Added tests for M0 and M10.

---
+++ .history/script_20200602104604.js
@@ -0,0 +1,32 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+import { Trend } from 'k6/metrics';
+import { check } from "k6";
+
+let uptosCount = new Trend('uptos_count');
+
+export let options = {
+  stages: [
+    { duration: '30s', target: 20 },
+    { duration: '1m30s', target: 10 },
+    { duration: '20s', target: 0 },
+  ],
+};
+
+function getRandomFloat(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+export default function() {
+  let lat = 41.388729386919316 + getRandomFloat(-0.0015, 0.0015);
+  let lng = 2.168776574948045 + getRandomFloat(-0.0015, 0.0015);
+  let radiusInKm = 1;
+  let res = http.get(`http://localhost:8080/uptos?lat=${lat}&lng=${lng}&radiusInKm=${radiusInKm}`);
+  check(res, {
+    "is status 200": (r) => r.status === 200
+  });
+  if (res.status === 200) {
+    uptosCount.add(JSON.parse(res.body).count);
+  }
+  sleep(1);
+}
